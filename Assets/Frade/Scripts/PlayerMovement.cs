1a8295d9decca9a488cfa1f2fd57fb58c6a16aff
Frade90 <franznyblin@gmail.com>
2022-12-15 16:05:50 +0200

    Update PlayerMovement.cs

--- Assets/Frade/Scripts/PlayerMovement.cs
+++ Assets/Frade/Scripts/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private LayerMask jumpableGround;
 
-    private float dirX = 0f;
+    
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
     
