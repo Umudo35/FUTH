dcdab0e2c1009293498cb53b87661c24ba17ee0d
Umudo35 <umut_ylmaz87@hotmail.com>
2023-04-28 02:50:10 +0300

    Latest Update Map

---
+++ Assets/Umut/Scripts/PlayerRespawn.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    public Vector3 respawnPoint;
+
+
+    public void RespawnNow()
+    {
+        transform.position = respawnPoint;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Death")
+        {
+            RespawnNow();
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
